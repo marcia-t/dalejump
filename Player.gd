b44c2d829d6a56900fd1ed42cd5b7e9f3b79d68c
marcia-t <tejedamarcia@gmail.com>
2018-09-09 15:42:43 -0300

    game working

--- Player.gd
+++ Player.gd
@@ -2,15 +2,11 @@ extends RigidBody2D
 var jump_speed = 650
 var speed = 300
 var sprite
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+
 
 func _ready():
 	sprite = get_node("Sprite")
 	set_physics_process(true)
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
 	pass
 
 func _physics_process(delta):
@@ -23,13 +19,9 @@ func _physics_process(delta):
 	if !Input.is_action_pressed("ui_left")  and !Input.is_action_pressed("ui_right"):
 		set_linear_velocity(Vector2(0, get_linear_velocity().y))
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
 
 
 func collision(body):
 	if body.is_in_group("Paddles") and get_linear_velocity().y > 0:
 		set_linear_velocity(Vector2(0, -jump_speed)) 
-	pass # replace with function body
+	pass 
