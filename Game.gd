85ef000db8a51c84eaa4d1d89dd2eb2cd28e9c03
marcia-t <tejedamarcia@gmail.com>
2018-09-09 21:10:16 -0300

    losing

--- Game.gd
+++ Game.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
 var step = preload('res://Step.tscn')
-var dimensions
+var camera 
+var width
 var y = 0
 var player 
 var score = 0
+var height
 
 func _ready():
-	dimensions = get_viewport_rect().size.x
+	width = get_viewport_rect().size.x
+	height = get_viewport_rect().size.y
 	player = get_node("Player")
+	camera = get_node("Camera")
 	randomize()
 	set_process(true)
 
@@ -18,8 +22,11 @@ func _process(delta):
 	#Ver si se puede obtener un nro en base a la distancia al player
 	y -= rand_range(0,190)
 	var new_step = step.instance()
-	new_step.position = Vector2(rand_range(-dimensions/2, dimensions/2),y)
+	new_step.position = Vector2(rand_range(-width/2, width/2),y)
 	add_child(new_step)
+	if (player.position.y > camera.position.y + height/2):
+		#aca mostrar game over screen
+		print("perdi")
 	pass
 
 #Sumar ptaje
